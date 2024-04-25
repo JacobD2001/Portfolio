cf0c72899e9af6e823c40e30e12216343162cff0
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-04-25 15:46:55 +0200

    added technologies to project details

--- src/app/components/project-details/project-details.component.ts
+++ src/app/components/project-details/project-details.component.ts
@@ -1,34 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Project } from 'src/app/interfaces/project';
+import { Skill } from 'src/app/interfaces/skill';
 import { Studies } from 'src/app/interfaces/studies';
 import { ProjectsService } from 'src/app/services/projects.service';
+import { SkillsService } from 'src/app/services/skills.service';
+
 
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
-  styleUrls: ['./project-details.component.scss']
+  styleUrls: ['./project-details.component.scss'],
 })
 export class ProjectDetailsComponent implements OnInit {
-
   id!: number;
-  storedTheme?: string | null = localStorage.getItem('theme-color') || 'theme-blue';
+  storedTheme?: string | null =
+    localStorage.getItem('theme-color') || 'theme-blue';
   projects?: Project[];
+  projectTechnologies: Skill[];
 
-  constructor(private route: ActivatedRoute, private projectsService: ProjectsService) { }
+
+  constructor(
+    private route: ActivatedRoute,
+    private projectsService: ProjectsService,
+    private skillsService: SkillsService
+  ) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
-    this.projects = this.getProjectById(this.id);
+    this.projects = this.projectsService.getProjectById(this.id);
+    this.projectTechnologies = this.skillsService.getSkillsByIds(this.projects[0].technologiesIds);
   }
 
-  getProjectById(id: number){
-    return this.projectsService.getProjectById(id);
-  }
-
-  changeColor(theme: string){
+  changeColor(theme: string) {
     localStorage.setItem('theme-color', theme);
-    this.storedTheme = localStorage.getItem('theme-color');;
+    this.storedTheme = localStorage.getItem('theme-color');
   }
-
 }

ed294f463cf2c8017b5146529783b73ba673e2fc
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-06-17 23:07:30 +0200

    certification

--- src/app/services/studies.service.ts
+++ src/app/services/studies.service.ts
@@ -13,19 +13,19 @@ export class StudiesService {
     {
       id: 1,
       title: 'MTA Security Fundamentals',
-      image: '../../assets/certifications/php.png',
+      image: '../../assets/certifications/security.jpg',
       link: 'https://www.credly.com/badges/97c20b3a-bc7c-4656-87d8-e52577332a96/public_url',
     },
     {
       id: 2,
       title: 'MTA Database Administration',
-      image: '../../assets/certifications/php.png',
+      image: '../../assets/certifications/database.jpg',
       link: 'https://www.credly.com/badges/385e97bd-e2ea-4997-89b5-aa714fef74e3/public_url',
     },
     {
       id: 3,
       title: 'MTA Software Development',
-      image: '../../assets/certifications/php.png',
+      image: '../../assets/certifications/software.jpg',
       link: 'https://www.credly.com/badges/ff9c7db0-2dc8-4f2e-8e15-fc84ec27f7ff/public_url',
     },
   ];
